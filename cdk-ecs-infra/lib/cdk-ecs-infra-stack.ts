468872da8255c030a6271d9f65dd765e583bd5f6
Diksha <76660222+diksha1159@users.noreply.github.com>
2023-02-22 12:10:07 +0000

    integrating codebase

---
+++ cdk-ecs-infra/lib/cdk-ecs-infra-stack.ts
@@ -0,0 +1,50 @@
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CdkEcsInfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+     // Look up the default VPC
+     const vpc = ec2.Vpc.fromLookup(this, "VPC", {
+      isDefault: true
+    });
+
+    const taskIamRole = new cdk.aws_iam.Role(this, "AppRole", {
+      roleName: "AppRole",
+      assumedBy: new cdk.aws_iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+    });
+
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'Task', {
+      taskRole: taskIamRole,
+    });
+
+    taskDefinition.addContainer('MyContainer', {
+      image: ecs.ContainerImage.fromAsset('../SampleApp'),
+      portMappings: [{ containerPort: 80 }],
+      memoryReservationMiB: 256,
+      cpu: 256,
+    });
+
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyApp", {
+      vpc: vpc,
+      taskDefinition: taskDefinition,
+      desiredCount: 1,
+      serviceName: 'MyWebApp',
+      assignPublicIp: true,
+      publicLoadBalancer: true,
+    })
+
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkEcsInfraQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
